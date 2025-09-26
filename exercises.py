43eb747d23117c30d88aca71a84395067a46b521
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-09-26 01:54:09 +0100

    added exercise 2

--- exercises.py
+++ exercises.py
@@ -10,11 +10,30 @@ app = FastAPI()
 class Hello(BaseModel):
     text: str
 
-@app.post("/hello")
+@app.post("/hello-llm")
 def hello(request:Hello):
     hello_pipeline = pipeline("text-generation", model="distilgpt2")
     greeting = hello_pipeline(request.text)
     return {
         "text": request.text,
         "Greetings": greeting
+    }
+
+## TEXT SUMMARIZER
+
+class summary(BaseModel):
+    long_text: str
+
+@app.post("/summarize")
+def summary(request:summary):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    summarized = summarizer(
+        request.long_text,
+        max_length=50,
+        min_length=15,
+        do_sample=False
+    )
+    return {
+        "Long Text": request.long_text,
+        "Summary": summarized
     }
