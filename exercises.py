a3797cd4b5e1177d5982c17244bf85d998c2ca3a
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-10-02 13:40:00 +0100

    added comments on code

--- exercises.py
+++ exercises.py
@@ -1,66 +1,69 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from transformers import pipeline
 from typing import Optional
-from pydantic import BaseModel
+from PIL import Image
+import io
 
 app = FastAPI()
 
 ## Hello LLM Endpoint
 
-class Hello(BaseModel):
-    text: str
-
 @app.post("/hello-llm")
-def hello(request:Hello):
+def hello(text: str):
     hello_pipeline = pipeline("text-generation", model="distilgpt2")
-    greeting = hello_pipeline(request.text)
+    greeting = hello_pipeline(text)
     return {
-        "text": request.text,
+        "text": text,
         "Greetings": greeting
     }
 
-## TEXT SUMMARIZER
+# ## TEXT SUMMARIZER
 
-class summary(BaseModel):
-    long_text: str
+# @app.post("/summarize")
+# def summary(long_text):
+#     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+#     summarized = summarizer(
+#         long_text,
+#         max_length=50,
+#         min_length=15,
+#         do_sample=False
+#     )
+#     return {
+#         "Long Text": long_text,
+#         "Summary": summarized
+#     }
 
-@app.post("/summarize")
-def summary(request:summary):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    summarized = summarizer(
-        request.long_text,
-        max_length=50,
-        min_length=15,
-        do_sample=False
-    )
-    return {
-        "Long Text": request.long_text,
-        "Summary": summarized
-    }
+# ## SENTIMENT ANALYSIS
 
-## SENTIMENT ANALYSIS
+# @app.post("/sentiment")
+# def senti(text: str):
+#     sentiment_pipeline = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
+#     senti = sentiment_pipeline(text)
 
-class Sentiment(BaseModel):
-    text: str
+#     return {
+#         "text": text,
+#         "sentiment": senti
+#     }
 
-@app.post("/sentiment")
-def senti(request:Sentiment):
-    sentiment_pipeline = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-    senti = sentiment_pipeline(request.text)
+# ## MULTIMODAL IMAGE CAPTIONING
 
-    return {
-        "text": request.text,
-        "sentiment": senti
-    }
+# @app.post("/caption-image")
+# async def run_file(file:UploadFile=File(...)):
+#     caption_pipeline = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+#     image_bytes = await file.read()
+#     image = Image.open(io.BytesIO(image_bytes))
+#     captioned_image = caption_pipeline(image)
+#     return {
+#         "file": file.filename,
+#         "caption": captioned_image
+#     }
 
-## MULTIMODAL IMAGE CAPTIONING
+# @app.post("/caption-image-link")
+# def caption_image_link(image_link: str):
+#     caption_pipeline = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+#     captioned_image = caption_pipeline(image_link)
+#     return {
+#       "file": image_link,
+#       "caption": captioned_image
+#     }
 
-@app.post("/caption-image")
-def caption_image(file:UploadFile=File(...)):
-    caption_pipeline = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
-    captioned_image = caption_pipeline(file)
-    
-    return {
-        "file": file.filename,
-        "caption": captioned_image
-    }
