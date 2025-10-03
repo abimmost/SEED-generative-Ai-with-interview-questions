2d237f27fb3f5134f7e98dd7b8d1054723b4fa94
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-10-03 17:48:25 +0100

    change

--- exercises.py
+++ exercises.py
@@ -11,8 +11,8 @@ class HelloRequest(BaseModel):
 @app.post("/hello-llm")
 def hello(request: HelloRequest):
     hello_pipeline = pipeline("text-generation", model="distilgpt2")
-    greeting = hello_pipeline(request.text)
+    greeting = hello_pipeline(request)
     return {
-        "text": request.text,
+        "text": request,
         "Greetings": greeting
     }
